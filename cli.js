32c21c1d3d5be563d78dbb277ad07be92cb81c70
engine <alexey@alexeys-air.mynet>
2023-06-12 18:04:45 +0100

    add cli.js and shebang to gendiff.js (execution file)

---
+++ cli.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { program } from 'commander';
+
+program
+  .description('Print a greeting')
+  .version('something')
+//   .action((options) => {
+//     const name = options.name || 'World';
+//     console.log(`Hello, ${name}!`);
+//   });
