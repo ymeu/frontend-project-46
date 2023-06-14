e0fba164251c7d3576d2a861a2d328f0f1521f02
engine <alexey@alexeys-air.mynet>
2023-06-14 14:03:05 +0100

    add chmod command to make gendiff file executable and update gendiff file to display -h

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse();
+
+const genDiff = (filepath1, filepath2) => {
+// Implementation to generate the difference between the files
+// and return it as a string
+};
+
+export default genDiff;
