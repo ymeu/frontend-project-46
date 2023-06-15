01641037304dbb1e1a286c0daa16ce016250c92f
engine <alexey@alexeys-air.mynet>
2023-06-15 16:05:36 +0100

    add format option to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
 
