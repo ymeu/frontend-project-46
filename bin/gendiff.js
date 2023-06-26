49469e38ced02491901e403b534cd163fc181785
engine <alexey@alexeys-air.mynet>
2023-06-26 15:02:18 +0100

    add fs.readFileSync to read files via full paths

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,15 +2,27 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { program } from 'commander';
+import fs from 'fs';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .arguments('[filepath1] [filepath2]')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
 
-program.parse();
+// program.parse();
+
+const [filepath1, filepath2] = program.args;
+
+const data1 = fs.readFileSync(filepath1, 'utf-8');
+console.log(data1);
+
+if (filepath2) {
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  console.log(data2);
+}
 
 const genDiff = (filepath1, filepath2) => {
 // Implementation to generate the difference between the files
@@ -18,3 +30,7 @@ const genDiff = (filepath1, filepath2) => {
 };
 
 export default genDiff;
+
+// gendiff __fixtures__/file1.json __fixtures__/file2.json
+// gendiff __fixtures__/file1.json
+// gendiff file1.json file2.json
