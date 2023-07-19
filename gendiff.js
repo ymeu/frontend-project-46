3f23fdfce7e60519c355a714936ec829074043c0
engine <alexey@alexeys-air.mynet>
2023-07-19 11:15:13 +0100

    refactor genDiff function

--- gendiff.js
+++ gendiff.js
@@ -1,25 +1,16 @@
 /* eslint-disable no-restricted-syntax */
 import fs from 'fs';
 import * as path from 'path'; // Import the 'path' module
+// eslint-disable-next-line import/no-extraneous-dependencies
 import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
-
-  const formatValue = (value) => {
-    if (typeof value === 'string') {
-      return `'${value}'`;
-    }
-    return value;
-  };
-
   const result = {};
 
   // resolved relative paths to absolute paths
   const workingDirectory = process.cwd();
   const resolvedPath1 = path.resolve(workingDirectory, filepath1);
-  const resolvedPath2 = filepath2
-    ? path.resolve(workingDirectory, filepath2)
-    : undefined;
+  const resolvedPath2 = path.resolve(workingDirectory, filepath2);
 
   // read contents of the files and parse them to return as string rathen than a JSON object
   const data1 = JSON.parse(fs.readFileSync(resolvedPath1, 'utf-8'));
@@ -31,24 +22,29 @@ const genDiff = (filepath1, filepath2) => {
 
   for (const key1 of keys1) {
     if (keys2.includes(key1)) {
-      if (_.isEqual(data1[key1], data2[key1])) {
-        result[key1] = `    ${key1}: ${formatValue(data1[key1])}`;
+      if (data1[key1] === data2[key1]) {
+        result[`    ${key1}`] = data1[key1];
       } else {
-        result[`-${key1}`] = `  - ${key1}: ${formatValue(data1[key1])}`;
-        result[`+${key1}`] = `  + ${key1}: ${formatValue(data2[key1])}`;
+        result[`  - ${key1}`] = data1[key1];
+        result[`  + ${key1}`] = data2[key1];
       }
     } else {
-      result[`-${key1}`] = `  - ${key1}: ${formatValue(data1[key1])}`;
+      result[`  - ${key1}`] = data1[key1];
     }
   }
 
   for (const key2 of keys2) {
     if (!keys1.includes(key2)) {
-      result[`+${key2}`] = `  + ${key2}: ${formatValue(data2[key2])}`;
+      result[`  + ${key2}`] = data2[key2];
     }
   }
 
-  console.log(`{\n${Object.values(result).join('\n')}\n}`);
+  const stringJSON = JSON.stringify(result)
+    .replace(/"/g, '') // removing quotes
+    .replace(/,/g, ',\n') // each key from new line
+    // eslint-disable-next-line newline-per-chained-call
+    .replace('{', '{\n').replace('}', '\n}'); // brackets on new lines
+  return console.log(stringJSON);
 };
 
 export default genDiff;
